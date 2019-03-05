acc39b18eeb62ccedecc7c1e4d4d8dda14802fe4
Learner <linhdqtg@gmail.com>
2019-03-06 03:42:20 +0700

    3rd mess
    1st try

--- src/CryptoNoteCore/Difficulty.h
+++ src/CryptoNoteCore/Difficulty.h
@@ -14,4 +14,11 @@ uint64_t nextDifficultyV4(std::vector<uint64_t> timestamps, std::vector<uint64_t
 
 uint64_t nextDifficultyV3(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties);
 
-uint64_t nextDifficulty(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties, const uint64_t blockHeight);
+uint64_t nextDifficulty(std::vector<uint64_t> timestamps, std::vector<uint64_t> cumulativeDifficulties);
+
+/* TODO: This has been added in the stdlib in c++17 */
+template <typename T>
+T clamp(const T& n, const T& lower, const T& upper)
+{
+    return std::max(lower, std::min(n, upper));
+}
